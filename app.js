97c89f5acfbde3412004157e90d78592b0cc6ff4
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-29 15:37:00 -0500

    populated index.html

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// DB Config
+// require("./config/db");
+
 const app = express();
 
 const poll = require("./routes/poll");
 
-// set public folder
+// Set public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-//body parser
+// Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// enable CORS
-app.use(cors);
+// Enable CORS
+app.use(cors());
 
 app.use("/poll", poll);
 
 const port = 3000;
 
-// start server
+// Start server
 app.listen(port, () => console.log(`Server started on port ${port}`));
